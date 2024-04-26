8c2807de79e6739391c19e2f852db6506bb34873
prakrist21 <159223080+prakrist21@users.noreply.github.com>
2024-04-26 11:25:50 +0545

    py code

---
+++ main.py
@@ -0,0 +1,19 @@
+f=open("match.csv","r")
+f1=open("matchreport.csv","w")
+next(f)
+f1.write("Team,Match_Played,Wins,Draw,Loss,Points")
+for x in f:
+    to=x.split(",")
+    print(to)
+    team=to[0]
+    win=to[1]
+    draw=to[2]
+    loss=int(to[3])
+    points=int(win)*3+int(draw)*1
+    matchplayed=int(win)+int(draw)+int(loss)
+    f1.write(f"\n{team},{matchplayed},{win},{draw},{loss},{points}")
+
+
+
+
+    
